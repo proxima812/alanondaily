ea1c2c991bb760215bd8838fdd240d056dcc5f7f
proxima812 <mrzuc2015@yandex.ru>
2024-05-11 11:43:12 +0500

    first commit

---
+++ src/settings.ts
@@ -0,0 +1,24 @@
+export const config = {
+	site: {
+		base: {
+			url: "https://alanon.vercel.app",
+		},
+		header: {
+			head: "Ежедневные размышления Ал-Анон",
+		},
+		OG: {
+			site_name: "12 steps",
+			author: "samgold",
+			title: "Daily 12 Steps",
+			description: "Daily 12 Steps diaries",
+			locale: "ru",
+			defaultImage: "default-ogImage.png",
+		},
+	},
+	verifications: {
+		yandex: "",
+		google: "",
+		bing: "",
+		// ...
+	},
+}
